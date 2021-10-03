ed7e2798e75ffff7e7c73584f64e553fbef96859
Jérémy Bochard <jeremy.bochard@gmail.com>
2021-10-03 16:42:16 +0200

    modify layout

--- js/images.js
+++ js/images.js
@@ -1,18 +1,11 @@
 function getBestMovieImg() {
-    fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=")
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-
+    fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
+        .then(response => response.json())
         .then(function (value) {
-            document
-                .querySelector(".best-movie-img")
-
-                .src = value.results[0].image_url;
+            document.getElementsByClassName("best-movie-img")[0].innerHTML += `
+            <img class="best_img" id="${value.results[0].id}" src="${value.results[0].image_url}"/>
+            `
         })
-
         .catch(function (err) {
             // Une erreur est survenue
         })
@@ -22,7 +15,6 @@ function getCarouselImgPage1(genre) {
     if (genre == "") {
         fetch("http://localhost:8000/api/v1/titles/?" + genre + "&sort_by=-imdb_score&page=1")
             .then(response => response.json())
-
             .then(function (response) {
                 response.results.forEach(function (value, index) {
                     document.getElementById('best-movie-img-' + index).innerHTML += `
@@ -31,14 +23,12 @@ function getCarouselImgPage1(genre) {
                     index++
                 })
             })
-
             .catch(function (err) {
                 // Une erreur est survenue
             })
     } else {
         fetch("http://localhost:8000/api/v1/titles/?genre=" + genre + "&sort_by=-imdb_score&page=1")
             .then(response => response.json())
-
             .then(function (response) {
                 response.results.forEach(function (value, index) {
                     document.getElementById(genre.toLowerCase() + '-movie-img-' + index).innerHTML += `
@@ -47,7 +37,6 @@ function getCarouselImgPage1(genre) {
                     index++
                 })
             })
-
             .catch(function (err) {
                 // Une erreur est survenue
             })
@@ -58,7 +47,6 @@ function getCarouselImgPage2(genre) {
     if (genre == "") {
         fetch("http://localhost:8000/api/v1/titles/?genre=" + genre + "&sort_by=-imdb_score&page=2")
             .then(response => response.json())
-
             .then(function (response) {
                 response.results.forEach(function (value, index) {
                     if (index < 2) {
@@ -69,14 +57,12 @@ function getCarouselImgPage2(genre) {
                     }
                 })
             })
-
             .catch(function (err) {
                 // Une erreur est survenue
             })
     } else {
         fetch("http://localhost:8000/api/v1/titles/?genre=" + genre + "&sort_by=-imdb_score&page=2")
             .then(response => response.json())
-
             .then(function (response) {
                 response.results.forEach(function (value, index) {
                     if (index < 2) {
@@ -87,7 +73,6 @@ function getCarouselImgPage2(genre) {
                     }
                 })
             })
-
             .catch(function (err) {
                 // Une erreur est survenue
             })
--- js/modal.js
+++ js/modal.js
@@ -2,34 +2,77 @@ function setup_modal() {
     // Get the modal
     let modal = document.getElementById("modal");
 
-    // Get the button that opens the modal
-    let btn = document.getElementById("open-modal");
-
     // Get the <span> element that closes the modal
     let span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     }
+}
+function fill_modal_best_movie() {
+    let modal = document.getElementById("modal")
+    let btn = document.getElementById("open-modal")
+    let image = document.querySelector(".best_img")
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+    let modal_image = document.querySelector(".img_container")
+    let title = document.querySelector("#movie-title")
+    let genre = document.querySelector("#movie-genre")
+    let publishedDate = document.querySelector("#movie-date-published")
+    let rated = document.querySelector("#movie-rated")
+    let imdb_score = document.querySelector("#movie-imdb-score")
+    let directors = document.querySelector("#movie-directors")
+    let actors = document.querySelector("#movie-actors")
+    let duration = document.querySelector("#movie-duration")
+    let countries = document.querySelector("#movie-countries")
+    let boxOffice = document.querySelector("#movie-box-office")
+    let description = document.querySelector("#movie-description")
+
+    btn.onclick = function () {
+        fetch("http://localhost:8000/api/v1/titles/" + image.id)
+            .then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(function (value) {
+                modal_image.innerHTML += `<img class="modal_img" "src="${value.image_url}/>"`
+                title.textContent = value.original_title
+                genre.textContent = value.genres.join(" - ")
+                publishedDate.textContent = date(value.date_published)
+                rated.textContent = value.rated
+                imdb_score.textContent = value.imdb_score + " / 10"
+                directors.textContent = "Directed by : " + value.directors.join(", ")
+                actors.textContent = "With " + value.actors.join(", ")
+                duration.textContent = value.duration + " min"
+                countries.textContent = value.countries.join(" - ")
+                boxOffice.textContent = value.worldwide_gross_income
+                description.textContent = value.long_description
+            })
+            .catch(function (err) {
+                // Une erreur est survenue
+            })
+        modal.style.display = "block";
     }
 }
 
-function fill_modal() {
-    let modal = document.getElementById("modal");
 
-    let images = document.querySelectorAll('.car_img')
+function fill_modal_carousel() {
+    let modal = document.getElementById("modal")
+    let images = document.querySelectorAll(".car_img")
+
+    let modal_image = document.querySelector(".img_container")
+    let title = document.querySelector("#movie-title")
+    let genre = document.querySelector("#movie-genre")
+    let publishedDate = document.querySelector("#movie-date-published")
+    let rated = document.querySelector("#movie-rated")
+    let imdb_score = document.querySelector("#movie-imdb-score")
+    let directors = document.querySelector("#movie-directors")
+    let actors = document.querySelector("#movie-actors")
+    let duration = document.querySelector("#movie-duration")
+    let countries = document.querySelector("#movie-countries")
+    let boxOffice = document.querySelector("#movie-box-office")
+    let description = document.querySelector("#movie-description")
 
     for (let i = 0; i < images.length; i++) {
         images[i].onclick = function () {
@@ -40,12 +83,18 @@ function fill_modal() {
                     }
                 })
                 .then(function (value) {
-                    document
-                        .querySelector("#movie-title")
-                        .textContent = value.title
-                    document
-                        .querySelector("#movie-genre")
-                        .textContent = value.genres
+                    modal_image.innerHTML += `<img class="modal_img" "src="${value.image_url}/>"`
+                    title.textContent = value.original_title
+                    genre.textContent = value.genres.join(" - ")
+                    publishedDate.textContent = date(value.date_published)
+                    rated.textContent = value.rated
+                    imdb_score.textContent = value.imdb_score + " / 10"
+                    directors.textContent = "Directed by : " + value.directors.join(", ")
+                    actors.textContent = "With " + value.actors.join(", ")
+                    duration.textContent = value.duration + " min"
+                    countries.textContent = value.countries.join(" - ")
+                    boxOffice.textContent = value.worldwide_gross_income
+                    description.textContent = value.long_description
                 })
                 .catch(function (err) {
                     // Une erreur est survenue
@@ -53,15 +102,16 @@ function fill_modal() {
             modal.style.display = "block";
         }
     }
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
 }
 
+function date(rawDate) {
+    splitDate = rawDate.split("-")
+    console.log()
+    return splitDate[2] + "/" + splitDate[1] + "/" + splitDate[0]
+
+}
 setup_modal()
 setTimeout(function () {
-    fill_modal();
+    fill_modal_carousel()
+    fill_modal_best_movie();
 }, 1000)
